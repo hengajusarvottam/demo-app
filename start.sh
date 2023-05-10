581f01b8226fad7cb7ab68966f1701f49eeeca7b
sarvottam <hengajusarvottam@gmail.com>
2023-05-10 12:29:52 +0545

    remove pid

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 sleep 5
-
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
 bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails s -b '0.0.0.0'
