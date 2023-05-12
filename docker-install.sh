306c734cab8436999ed3d325235640f2e833b1b5
sarvottam <hengajusarvottam@gmail.com>
2023-05-12 15:54:45 +0545

    some changes

--- docker-install.sh
+++ docker-install.sh
@@ -54,3 +54,42 @@ Docker-compose Version
 **********************
 "
 docker-compose --version
+
+
+echo "
+*********************************************
+Start to install-Vagrant
+*********************************************
+"
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vagrant
+
+echo "
+***************
+Vagrant Version
+***************
+"
+vagrant --version
+
+echo "
+***************
+Download box with ubuntu image
+***************
+"
+sudo vagrant box add bento/ubuntu-22.04
+
+echo "
+***************
+Vagrant box list
+***************
+"
+vagrant box list
+
+echo "
+***************
+Vagrant box list
+***************
+"
+sudo apt install virtualbox -y 
+
